2add65f7da52247a4952f92a16fdcb1685b17bb5
Pavi99 <pavithra@smbapps.com.au>
2022-12-30 20:54:29 +0530

    rest api calls committed

--- backend/src/main/java/com/springbootbackend/backend/controller/StudentController.java
+++ backend/src/main/java/com/springbootbackend/backend/controller/StudentController.java
@@ -22,19 +22,19 @@ public class StudentController {
         return studentRepository.findAll();
     }
 
-    @PostMapping("/createStudent")
+    @PostMapping("/students")
     public Student createStudent(@RequestBody Student student) {
         return studentRepository.save(student);
     }
 
-    @GetMapping("/student/{id}")
+    @GetMapping("/students/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id :" + id));
         return ResponseEntity.ok(student);
     }
 
-    @PutMapping("/student/{id}")
+    @PutMapping("/students/{id}")
     public ResponseEntity<Student> updateEmployee(@PathVariable Long id, @RequestBody Student studentDetails){
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
@@ -47,7 +47,7 @@ public class StudentController {
         return ResponseEntity.ok(updatedStudent);
     }
 
-    @DeleteMapping("/student/{id}")
+    @DeleteMapping("/students/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteStudent(@PathVariable Long id){
         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
